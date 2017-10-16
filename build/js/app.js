3aaf80d022160cc52fea727867a65716f3793fce
Javier Jimenez and Nate McGregor <nate@>
2017-10-16 16:34:05 -0700

    add files

--- build/js/app.js
+++ build/js/app.js
@@ -34,8 +34,33 @@ $(document).ready(function () {
   // });
   var city = new _simcity.Simcity();
   for (var i = 0; i < city.grid.length; ++i) {
-    $('#map1').append("<img src='img/grass.jpg' id='block" + city.grid[i] + "'>");
+    $('#map1').append("<img src='img/grass.jpg' class='block" + i + "' class='block' >");
   }
+  var theBlock = void 0;
+  $('.block').click(function (e) {
+    theBlock = links[$(e.currentTarget).children().attr('id')];
+  });
+
+  console.log(theBlock);
+
+  // $( "#block" ).click(function() {
+  //
+  // }
+  // $( "#farm" ).click(function() {
+  //
+  // }
+  // $( "#business" ).click(function() {
+  //
+  // }
+  // $( "#house" ).click(function() {
+  //
+  // }
+  // $( "#hotel" ).click(function() {
+  //
+  // }
+  // $( "#opera" ).click(function() {
+  //
+  // }
 
   setInterval(function () {});
 });
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -6,8 +6,33 @@ $(document).ready(function() {
   // });
   let city = new Simcity();
   for(let i = 0; i < city.grid.length ; ++i){
-    $('#map1').append("<img src='img/grass.jpg' id='block" + city.grid[i] + "'>")
+    $('#map1').append("<img src='img/grass.jpg' class='block" + i + "' class='block' >")
   }
+  let theBlock;
+  $('.block').click(function(e) {
+    theBlock = links[$(e.currentTarget).children().attr('id')];
+  });
+
+  console.log(theBlock)
+
+  // $( "#block" ).click(function() {
+  //
+  // }
+  // $( "#farm" ).click(function() {
+  //
+  // }
+  // $( "#business" ).click(function() {
+  //
+  // }
+  // $( "#house" ).click(function() {
+  //
+  // }
+  // $( "#hotel" ).click(function() {
+  //
+  // }
+  // $( "#opera" ).click(function() {
+  //
+  // }
 
   setInterval(function(){
   });
